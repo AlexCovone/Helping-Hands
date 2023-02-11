9c6a6080a0e8bcabf8e1588f65e6dae907352b66
Alex Covone <alexcovone@gmail.com>
2023-02-11 17:12:58 -0500

    Added success and error flash messages upon reservation request

--- controllers/events.js
+++ controllers/events.js
@@ -32,7 +32,8 @@ module.exports = {
   getEvent: async (req, res) => {
       try {
       const event = await Event.findById(req.params.id);
-      res.render("event.ejs", { event: event, user: req.user });
+      const message = req.flash()
+      res.render("event.ejs", { event: event, user: req.user, message: message });
       } catch (err) {
       console.log(err);
       }
@@ -54,19 +55,17 @@ module.exports = {
 
       // If req.user.id does not exist in a sub-array, push user's information into staffReserved array indicated user reserved event.
       if(staffReserved.some(subArr => subArr.includes(req.user.id))){
-        console.log('User has already reserved this event.')
+        req.flash("error", `${req.user.name} has already reserved this event.`);
       }else{
         await Event.findOneAndUpdate(
           {_id: req.params.id},
           {
             $push: {staffReserved: {$each: [[req.user.id,req.user.name, req.user.email, req.body.occupationRole]]}}
-          },
-          console.log(`Reservation has been made for ${req.user.name}.`)
+          }
         );
-      }
 
-      // Console logs list of all reserved users for specified event
-      console.log(staffReserved)
+        req.flash("success", `Reservation has been made for ${req.user.name}.`);
+      }
       
       // Conditional: Decrement Waitstaff/Bartender/Chef property on Event Model based on reservation request value 
       if(req.body.occupationRole === 'Waitstaff'){
@@ -91,40 +90,13 @@ module.exports = {
           }
         )
       }
-      
-      res.redirect(`/events/${req.params.id}`);
+
+      return res.redirect(`/events/${req.params.id}`);
     } catch (err) {
       console.log(err);
     }
   },
-  // createEvent: async (req, res) => {
-  //     try {
-  //       // Upload image to cloudinary
-  //       const result = await cloudinary.uploader.upload(req.file.path);
-  
-  //       await Event.create({
-  //         user: req.user.id,
-  //         eventName: req.body.eventName,
-  //         address: req.body.address,
-  //         image: result.secure_url,
-  //         cloudinaryId: result.public_id,
-  //         staffArrival: req.body.staffArrival,
-  //         estimatedEndTime: req.body.estimatedEndTime,
-  //         date: req.body.date,
-  //         uniform: req.body.uniform,
-  //         caterer: req.body.caterer,
-  //         eventCaptain: req.body.eventCaptain,
-  //         eventChef: req.body.eventChef,
-  //         waitstaffNeeded: req.body.waitstaffNeeded,
-  //         bartenderNeeded: req.body.bartenderNeeded,
-  //         chefNeeded: req.body.chefNeeded
-  //       });
-  //       console.log("Event has been added!");
-  //       res.redirect("/profile");
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  // },
+
   deleteEvent: async (req, res) => {
       try {
         // Find post by id - Make sure that the post exist (Caution using delete method)
