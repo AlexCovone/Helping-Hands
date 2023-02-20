d1763ea57bd5116f8c2019626570309228dacff7
Alex Covone <alexcovone@gmail.com>
2023-02-20 16:47:14 -0500

    Added userReservedEvents function

--- controllers/services/user.service.js
+++ controllers/services/user.service.js
@@ -1,4 +1,5 @@
 const User = require("../../models/User");
+const Event = require("../../models/Event");
 
 module.exports = {
 // Find appropriate user and push eventId onto eventReserved property
@@ -7,5 +8,18 @@ module.exports = {
             { _id: userId},
             { $push: { eventReserved: eventId } }
         )
+    },
+    userReservedEvents: async (userId) => {
+        const events = await Event.find();
+
+        // Filter through Events collection and loop through staffReserved property
+        const matchedEvents = events.filter(event => {
+            for(let i = 0; i < event.staffReserved.length; i++){
+                if(event.staffReserved[i][0] === userId){
+                    return event
+                }
+            }
+        })
+        return matchedEvents
     }
 }
