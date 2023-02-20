0063b9568b29bdbfa0765e3533413221ee14f19b
Alex Covone <alexcovone@gmail.com>
2023-02-20 14:42:31 -0500

    Deleted extra return statement

--- controllers/services/admin.service.js
+++ controllers/services/admin.service.js
@@ -2,8 +2,6 @@ module.exports = {
     formatPhoneNumber: (phoneNumbers) => {
         const validPhoneNumbers = phoneNumbers.filter(number => number.length === 12);
 
-        const e164Format = validPhoneNumbers.map(number => '+1' + number.split('-').join(''))
-
-        return e164Format
+        return validPhoneNumbers.map(number => '+1' + number.split('-').join(''))
     }
 }
